1b59a2738ec8b2396a1e6a38ed6b07e99630578c
mengpeilee <asdfg3865@gmail.com>
2018-04-28 16:50:06 +0800

    connect Router

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import {
-  View
+  View,
+  Text
 } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 class Login extends Component {
+  onClick() {
+    Actions.teamInfo();
+  }
+
   render() {
     return (
-      <View style={styles.container} />
+      <View style={styles.container}>
+        <Text onPress={this.onClick}>TeamInfo</Text>
+      </View>
     );
   }
 }
