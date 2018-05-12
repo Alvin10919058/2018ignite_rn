9434025def93086e02aeb7c7e74c8796b08b6967
Alvin10919058 <zero840108@gmail.com>
2018-05-12 22:04:13 +0800

    新增auth功能

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -4,3 +4,11 @@
  */
 
  export * from './BackgroundImage';
+ export * from './Button';
+ export * from './Card';
+ export * from './CardSection';
+ export * from './Header';
+ export * from './Input';
+ export * from './Spinner';
+ export * from './Confirm';
+ 
