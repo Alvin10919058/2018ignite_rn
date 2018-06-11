e34d9b4fff74b82fe65aaabcaed01603c69158df
mengpeilee <asdfg3865@gmail.com>
2018-06-12 00:34:35 +0900

    新增Begin頁面

---
+++ src/components/Begin.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Begin extends Component {
+  render() {
+    return (
+      <View style={styles.containerStyle} />
+    );
+  }
+}
+
+const styles = {
+  containerStyle: {
+    flex: 1
+  }
+};
+
+export default Begin;
