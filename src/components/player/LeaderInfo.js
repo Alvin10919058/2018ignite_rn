ab66afb5243558bf573906a14296723b1d5d74f5
mengpeilee <asdfg3865@gmail.com>
2018-06-12 17:24:26 +0900

    將LeaderInfo綁定Rudux

--- src/components/player/LeaderInfo.js
+++ src/components/player/LeaderInfo.js
@@ -5,13 +5,15 @@ import {
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Parse from 'parse/react-native';
+import { connect } from 'react-redux';
+import { getTeamData } from '../../actions';
 import { BackgroundImage } from '../common';
 //import data from '../../Setting.json';
 
 class LeaderInfo extends Component {
 
   componentWillMount() {
-
+    this.props.getTeamData();
   }
 
   logout() {
@@ -45,4 +47,38 @@ const styles = {
   }
 };
 
-export default LeaderInfo;
+const mapStateToProps = ({ player }) => {
+  const {  //國高能力值
+    strength, //力量
+    wisdom, //智慧
+    vitality, //體力
+    faith, //信心
+    agility, //敏捷
+
+    //大專能力值
+    passion, //熱情
+    creativity, //創意
+    intelligence, //智慧
+    love, //愛心
+    patience//耐力
+   } = player;
+
+  return { 
+    strength, //力量
+    wisdom, //智慧
+    vitality, //體力
+    faith, //信心
+    agility, //敏捷
+
+    //大專能力值
+    passion, //熱情
+    creativity, //創意
+    intelligence, //智慧
+    love, //愛心
+    patience//耐力
+  };
+};
+
+export default connect(mapStateToProps, {
+  getTeamData
+})(LeaderInfo);
