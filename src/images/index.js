68fcccb02154a06716847d5f9d910565fd4eb58e
mengpeilee <asdfg3865@gmail.com>
2018-06-12 15:38:42 +0900

    加入職業角色圖

--- src/images/index.js
+++ src/images/index.js
@@ -7,8 +7,15 @@ import SettingTab from './Setting.png';
 import MisionTab from './Mission.png';
 import HomePageTab from './HomePage.png';
 import SkillsTab from './Skills.png';
+import Biochemical from './Biochemical.jpg';
+import Defense from './Defense.jpg';
+import Sniper from './Sniper.jpg';
+import Special from './Special.jpg';
+import Soldier from './Soldier.jpg';
+import Assault from './Assault.jpg';
 
 export {
   Background, Logo, User, Password,
-  RankTab, SettingTab, MisionTab, HomePageTab, SkillsTab
+  RankTab, SettingTab, MisionTab, HomePageTab, SkillsTab,
+  Biochemical, Defense, Sniper, Special, Soldier, Assault
 };
