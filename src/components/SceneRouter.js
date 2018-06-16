d1ca3f8be182e1ebdc10dc1adb30790c688ee360
mengpeilee <asdfg3865@gmail.com>
2018-06-16 22:20:33 +0800

    新增gm initial頁面

--- src/components/SceneRouter.js
+++ src/components/SceneRouter.js
@@ -52,7 +52,7 @@ class RouterComponent extends Component {
               <Scene key="tab5" title="排名" icon={TabIcon}>
                 <Scene key="rank2" initial component={Rank} hideNavBar />
               </Scene>
-              <Scene key="tab6" title="配點" icon={TabIcon}>
+              <Scene key="tab6" inital title="配點" icon={TabIcon}>
                 <Scene key="giveScore" initial component={GiveScore} hideNavBar />
               </Scene>
               <Scene key="tab7" title="設定" icon={TabIcon}>
