26576c26f1958298d4556543b56d3cd3cccc5e95
Alvin10919058 <zero840108@gmail.com>
2018-06-17 07:25:42 +0800

    新增 switch切換梯次功能 及picker-select範例

--- src/components/gm/GiveScore.js
+++ src/components/gm/GiveScore.js
@@ -1,19 +1,229 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React from 'react';
+import { Alert, Text, TextInput, StyleSheet, View, Switch } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-class GiveScore extends Component {
-  render() {
-    return (
-      <View style={styles.containerStyle} />
-    );
-  }
+class GiveScore extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.inputRefs = {};
+
+    this.state = {
+        switchType: true,
+        favColor: undefined,
+        items: [
+            {
+                label: 'Red',
+                value: 'red',
+            },
+            {
+                label: 'Orange',
+                value: 'orange',
+            },
+            {
+                label: 'Blue',
+                value: 'blue',
+            },
+        ],
+        favSport: undefined,
+        items2: [
+            {
+                label: 'Football',
+                value: 'football',
+            },
+            {
+                label: 'Baseball',
+                value: 'baseball',
+            },
+            {
+                label: 'Hockey',
+                value: 'hockey',
+            },
+        ],
+    };
 }
 
-const styles = {
-  containerStyle: {
-    flex: 1,
-    backgroundColor: 'white'
-  }
-};
+componentDidMount() {
+    // if the component is using the optional `value` prop, the parent
+    // has the abililty to both set the initial value and also update it
+    setTimeout(() => {
+        this.setState({
+            favColor: 'red',
+        });
+    }, 1000);
+
+    // parent can also update the `items` prop
+    setTimeout(() => {
+        this.setState({
+            items: this.state.items.concat([{ value: 'purple', label: 'Purple' }]),
+        });
+    }, 2000);
+}
+
+render() {
+    return (
+        <View style={styles.container}>
+              {
+                this.state.switchType === true
+                &&
+                <Text>國高</Text>
+              }
+               {
+                this.state.switchType === false
+                &&
+                <Text>大專</Text>
+              }
+            <Switch
+              onValueChange={(value) => {
+                this.setState({ switchType: value });
+              }}
+              style={styles.switchButton}
+              value={this.state.switchType}
+            />
+            <Text>Name?</Text>
+            <RNPickerSelect
+                placeholder={{
+                    label: 'Select a color...',
+                    value: null,
+                }}
+                items={this.state.items}
+                onValueChange={(value) => {
+                    this.setState({
+                        favColor: value,
+                    });
+                }}
+                onUpArrow={() => {
+                    this.inputRefs.name.focus();
+                }}
+                onDownArrow={() => {
+                    this.inputRefs.picker2.togglePicker();
+                }}
+                style={{ ...pickerSelectStyles }}
+                value={this.state.favColor}
+                ref={(el) => {
+                    this.inputRefs.picker = el;
+                }}
+            />
+
+            <View style={{ paddingVertical: 5 }} />
+
+            <Text>What&rsquo;s your favorite color?</Text>
+            <RNPickerSelect
+                placeholder={{
+                    label: 'Select a color...',
+                    value: null,
+                }}
+                items={this.state.items}
+                onValueChange={(value) => {
+                    this.setState({
+                        favColor: value,
+                    });
+                }}
+                onUpArrow={() => {
+                    this.inputRefs.name.focus();
+                }}
+                onDownArrow={() => {
+                    this.inputRefs.picker2.togglePicker();
+                }}
+                style={{ ...pickerSelectStyles }}
+                value={this.state.favColor}
+                ref={(el) => {
+                    this.inputRefs.picker = el;
+                }}
+            />
+
+            <View style={{ paddingVertical: 5 }} />
+
+            <Text>What&rsquo;s your favorite sport?</Text>
+            <RNPickerSelect
+                placeholder={{
+                    label: 'Select a sport...',
+                    value: null,
+                }}
+                items={this.state.items2}
+                onValueChange={(value) => {
+                    this.setState({
+                        favSport: value,
+                    });
+                }}
+                onUpArrow={() => {
+                    this.inputRefs.picker.togglePicker();
+                }}
+                onDownArrow={() => {
+                    this.inputRefs.company.focus();
+                }}
+                style={{ ...pickerSelectStyles }}
+                value={this.state.favSport}
+                ref={(el) => {
+                    this.inputRefs.picker2 = el;
+                }}
+            />
+
+            <View style={{ paddingVertical: 5 }} />
+
+              {/* <Text>Company?</Text>
+              <TextInput
+                  ref={(el) => {
+                      this.inputRefs.company = el;
+                  }}
+                  returnKeyType="go"
+                  enablesReturnKeyAutomatically
+                  style={pickerSelectStyles.inputIOS}
+                  onSubmitEditing={() => {
+                      Alert.alert('Success', 'Form submitted', [{ text: 'Okay', onPress: null }]);
+                  }}
+              /> */}
+        </View>
+    );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+      paddingTop: 30,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      paddingHorizontal: 10,
+  },
+  switchButton: {
+    alignSelf: 'center',
+    transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }]
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+      fontSize: 16,
+      paddingTop: 13,
+      paddingHorizontal: 10,
+      paddingBottom: 12,
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 4,
+      backgroundColor: 'white',
+      color: 'black',
+  },
+});
 
 export default GiveScore;
+
+
+// import React, { Component } from 'react';
+// import { View } from 'react-native';
+
+// class GiveScore extends Component {
+//   render() {
+//     return (
+//       <View style={styles.containerStyle} />
+//     );
+//   }
+// }
+
+// const styles = {
+//   containerStyle: {
+//     flex: 1,
+//     backgroundColor: 'white'
+//   }
+// };
+
+// export default GiveScore;
