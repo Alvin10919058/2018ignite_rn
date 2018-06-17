1f720f0f1e2da937d4cfdb2194ba7c2c6339fe6f
Alvin10919058 <zero840108@gmail.com>
2018-06-17 17:49:14 +0800

    完成gm配分紀錄表格

--- src/components/gm/GiveScore.js
+++ src/components/gm/GiveScore.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Alert, Text, StyleSheet, View } from 'react-native';
+import { Alert, Text, StyleSheet, View, ScrollView, AsyncStorage } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
+import { Table, Row } from 'react-native-table-component';
 import { Button, Spinner } from '../common';
 import SwitchButton from '../common/SwitchButton';
+import data from '../../Setting.json';
+
 
 class GiveScore extends React.Component {
   constructor(props) {
@@ -13,6 +16,12 @@ class GiveScore extends React.Component {
     this.state = {
         activeSwitch: 1,
         selecTeam: undefined,
+
+        TeamData: {},
+        tableHead: ['梯次', '小隊', '種類', '點數'],
+        tableData: [],
+        widthArr: [80, 85, 120, 85],
+
         teams: [
             {
                 label: 'Team01',
@@ -268,6 +277,7 @@ class GiveScore extends React.Component {
 }
 
 componentDidMount() {
+  this.getGMdata();
     // if the component is using the optional `value` prop, the parent
     // has the abililty to both set the initial value and also update it
     // setTimeout(() => {
@@ -284,9 +294,166 @@ componentDidMount() {
     // }, 2000);
 }
 
+  //GET這個GM寫了幾筆資料
+  async getGMdata() {
+    const sessionToken = await AsyncStorage.getItem('sessionToken');
+    const userID = await AsyncStorage.getItem('userID');
+
+    const params = {
+      include: 'to_team',
+      limit: 1000,
+      where: {
+          from_gm: {
+              __type: 'Pointer',
+              className: '_User',
+              objectId: userID
+          },
+      }
+    };
+    const esc = encodeURIComponent;
+    const query = Object.keys(params)
+        .map(k => `${esc(k)}=${esc(JSON.stringify(params[k]))}`)
+        .join('&');
+    fetch(`${data.parseServerURL}/classes/Point?${query}`, {
+    method: 'GET',
+    headers: {
+        'X-Parse-Application-Id': data.parseAppId,
+        'X-Parse-REST-API-Key': data.paresApiKey,
+        'X-Parse-Session-Token': sessionToken
+    }
+    })
+    .then((response) => response.json())
+    .then(async (responseData) => {
+         //將取得的物件資料複製一份到obj
+         const CopyRes = [...responseData.results];
+        
+         //將此物件依據小隊總分由大到小(b-a)的順序來排序
+         CopyRes.sort((a, b) => b.updatedAt - a.updatedAt);
+        
+         this.setState({ PointData: CopyRes });
+
+         //選出要的資料放入tableData中
+           const finalDataAry = [];
+          
+           CopyRes.map((PointData) => {
+             finalDataAry.push([PointData.batch, PointData.to_team.name, PointData.kinds, PointData.value]);
+            console.log(finalDataAry);
+             return true;
+           });
+     this.setState({ tableData: finalDataAry });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+  //value: 要給幾點, kinds: 哪種類別
+  getTeam(batch, teamName, value, kinds) {
+    const params = {
+        where: {
+          batch,
+          name: teamName
+        }
+    };
+    const esc = encodeURIComponent;
+    const query = Object.keys(params)
+        .map(k => `${esc(k)}=${esc(JSON.stringify(params[k]))}`)
+        .join('&');
+    fetch(`${data.parseServerURL}/classes/Team?${query}`, {
+    method: 'GET',
+    headers: {
+        'X-Parse-Application-Id': data.parseAppId,
+        'X-Parse-REST-API-Key': data.paresApiKey,
+    }
+    })
+    .then((response) => response.json())
+    .then(async (responseData) => {
+        console.log(responseData);
+        console.log(responseData.results[0][kinds]);
+        this.putTeam(
+          responseData.results[0].objectId, 
+          batch, 
+          value, 
+          kinds, 
+          responseData.results[0][kinds]
+        );
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+  //value: 要給幾點, kinds: 哪種類別, originalValue: 原來類別的分數
+  putTeam(teamID, batch, value, kinds, originalValue) {
+    const params = {
+        
+    };
+
+    params[kinds] = originalValue + value;
+    
+    fetch(`${data.parseServerURL}/classes/Team/${teamID}`, {
+    method: 'PUT',
+    headers: {
+        'X-Parse-Application-Id': data.parseAppId,
+        'X-Parse-REST-API-Key': data.paresApiKey,
+    },
+    body: JSON.stringify(params)
+    })
+    .then((success) => {
+      console.log(success);
+      this.postPoint(teamID, batch, value, kinds);
+    })
+    .catch((err) => {
+    console.log(err);// error handling ..
+    });
+  }
+
+  //value: 要給幾點, kinds: 哪種類別
+  async postPoint(teamID, batch, value, kinds) {
+    const sessionToken = await AsyncStorage.getItem('sessionToken');
+    const userID = await AsyncStorage.getItem('userID');
+
+    const params = {
+      to_team: {
+          __type: 'Pointer',
+          className: 'Team',
+          objectId: teamID
+        },
+      from_gm: {
+          __type: 'Pointer',
+          className: '_User',
+          objectId: userID
+      },
+
+      ACL: {},
+      kinds,
+      batch,
+      value
+    };
+
+    params.ACL[userID] = { read: true, write: true };
+    params.ACL['*'] = { read: true };
+    
+    fetch(`${data.parseServerURL}/classes/Point/`, {
+    method: 'POST',
+    headers: {
+        'X-Parse-Application-Id': data.parseAppId,
+        'X-Parse-REST-API-Key': data.paresApiKey,
+        'X-Parse-Session-Token': sessionToken
+    },
+    body: JSON.stringify(params)
+    })
+    .then((success) => {
+    console.log(success);
+    })
+    .catch((err) => {
+    console.log(err);// error handling ..
+    });
+  }
+
 render() {
     return (
-     
+     <View style={{ flex: 1, backgroundColor: '#fff' }}>
         <View style={styles.container}>
             <SwitchButton
                     onValueChange={(val) => this.setState({ activeSwitch: val })}     
@@ -300,11 +467,12 @@ render() {
                     switchBorderColor='#d4d4d4'       
                     switchBackgroundColor='#fff'   
                     btnBorderColor='#00a4b9'          
-                    btnBackgroundColor='#00bcd4'     
+                    btnBackgroundColor='#69aeb2'     
                     fontColor='#b1b1b1'             
                     activeFontColor='#fff'
             />
-            <Text>小隊</Text>
+            <View style={{ paddingVertical: 10 }} />
+            <Text style={{ fontSize: 20, fontWeight: '400', marginBottom: 5 }}>小隊</Text>
             <RNPickerSelect
                 placeholder={{
                     label: '請選擇小隊',
@@ -328,7 +496,7 @@ render() {
                 {console.log()}
             <View style={{ paddingVertical: 5 }} />
 
-            <Text>種類</Text>
+            <Text style={{ fontSize: 20, fontWeight: '400', marginBottom: 5 }}>種類</Text>
             <RNPickerSelect
                 placeholder={{
                     label: '請選擇配點種類',
@@ -355,7 +523,7 @@ render() {
 
             <View style={{ paddingVertical: 5 }} />
 
-            <Text>點數</Text>
+            <Text style={{ fontSize: 20, fontWeight: '400', marginBottom: 5 }}>點數</Text>
             <RNPickerSelect
                 placeholder={{
                     label: '請選擇給予點數',
@@ -382,8 +550,37 @@ render() {
             <Button onPress={() => { console.log(this.state.activeSwitch, this.state.selecTeam, this.state.selecT1Kinds, this.state.selecNumber); }} >
               送出
             </Button>
-             
         </View>
+        <View style={{ paddingVertical: 5 }} />
+        <View>
+          <Table borderStyle={{ borderWidth: 2, borderColor: '#f0f0f0' }}>
+            <Row 
+              data={this.state.tableHead} 
+              widthArr={this.state.widthArr} 
+              style={styles.head} 
+              textStyle={styles.headText} 
+            />
+          </Table>
+        
+          <ScrollView style={styles.dataWrapper}>
+            <Table 
+              borderStyle={{ borderWidth: 2, borderColor: '#f0f0f0' }}
+            >
+              {
+                this.state.tableData.map((rowData, index) => (
+                  <Row
+                    key={index}
+                    data={rowData}
+                    widthArr={this.state.widthArr}
+                    style={[styles.row, index % 2 && { backgroundColor: '#bbc3dc' }]}
+                    textStyle={styles.text}
+                  />
+                ))
+              }
+            </Table>
+          </ScrollView>
+        </View>
+    </View>
     );
 }
 }
@@ -395,6 +592,30 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
       paddingHorizontal: 10,
+  },
+ 
+  head: { 
+    height: 40,
+    backgroundColor: '#555555'
+  },
+  text: { 
+    margin: 6,
+    textAlign: 'center', 
+    fontWeight: '400' 
+  },
+  headText: {
+    fontSize: 16,
+    margin: 6,
+    textAlign: 'center', 
+    fontWeight: '600',
+    color: 'white'
+  },
+  dataWrapper: { 
+    marginTop: -1 
+  },
+  row: { 
+    height: 40, 
+    backgroundColor: '#d2e9ff' 
   }
 });
 
