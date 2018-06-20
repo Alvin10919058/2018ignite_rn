dc929328c08c1735d6f85e116e1481c261d4ae2e
Alvin10919058 <zero840108@gmail.com>
2018-06-20 09:27:56 +0800

    優化 setting頁面 樣式 調整cardlist component

--- src/components/common/CardList.js
+++ src/components/common/CardList.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, Image, Dimensions } from 'react-native';
+import { chevronRight } from '../../images';
 
+const { height, width } = Dimensions.get('window');
 
 const CardList = ({ cardText, onPress, listCustomStyle, listTextStyle }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={listCustomStyle} >
+    <TouchableOpacity onPress={onPress} style={[listCustomStyle, { flexDirection: 'row', width: width * 1, justifyContent: 'space-between' }]} >
         <Text style={[styles.cardText, listTextStyle]}>{cardText}</Text>
+        <Image
+            style={{ 
+              width: 30, 
+              height: 30,
+              alignSelf: 'center'
+            }}
+            source={chevronRight}
+        />
     </TouchableOpacity>
   );
 };
